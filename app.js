e1c835c149475973a85d99f3bf73332b1cc45e76
Jhuly <137408725+Jhuly@users.noreply.github.com>
2024-11-11 10:50:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 alert("Boas-vindas ao jogo do número secreto") 
-let numeroSecreto = 88; let chute; let tentativas = 3; 
-//enquanto = while while(chute != numeroSecreto)
-{ chute = prompt("Escolha um número entre 1 e 10"); 
-    if(chute == numeroSecreto){ break } 
-    else{ if(numeroSecreto> chute){ 
-        alert(`O número secreto é maior que ${chute}`); } 
-        else { alert(`O número secreto é menor que ${chute}`); } }
-         tentativa++; if(tentativa>1)
-{ alert(`Parabéns!! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`); } 
-    else{ alert(`Parabéns!! Você acertou o número secreto ${numeroSecreto} com ${tentativa} tentativa`); } }
- let palavraTentativa = tentativa > 1 ? "tentativas": "tentativa" ;
+let numeroSecreto = 88; 
+let chute = prompt("Escolha um número entre 1 e 100"); 
+
+while(  chute!== numeroSecreto){
+    if(chute == numeroSecreto){ 
+        console.log(`Isso aí, você descobriu o número secreto ${numeroSecreto}`);
+     } 
+     else {
+
+        alert("Você errou!")
+     }
+    
+    }
